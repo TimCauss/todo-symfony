699677aea2524ff01b0ba0d33da5b32977d3b39c
TimCauss <t.caussignac@gmail.com>
2023-08-03 15:39:56 +0200

    Flex module final

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -13,18 +13,18 @@ class VinylController extends AbstractController
     public function homepage(): Response
     {
 
-        $tracks = [
-            ['song' => 'Gangaga\'s Paradise', 'artist' => 'Coolio'],
-            ['song' => 'Waterfalls', 'artist' => 'TLC'],
-            ['song' => 'Creep', 'artist' => 'Radiohead'],
-            ['song' => 'Kiss from a Rose', 'artist' => 'Seal'],
-            ['song' => 'On Bended Knee', 'artist' => 'Boyz II Men'],
-            ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
+        $tasks = [
+            ['name' => 'Gangaga\'s Paradise', 'status' => 'Coolio'],
+            ['name' => 'Waterfalls', 'status' => 'TLC'],
+            ['name' => 'Creep', 'status' => 'Radiohead'],
+            ['name' => 'Kiss from a Rose', 'status' => 'Seal'],
+            ['name' => 'On Bended Knee', 'status' => 'Boyz II Men'],
+            ['name' => 'Fantasy', 'status' => 'Mariah Carey'],
         ];
 
         return $this->render('vinyl/homepage.html.twig', [
             'title' => 'Ceci est la variable title',
-            'tracks' => $tracks,
+            'tasks' => $tasks,
         ]);
     }
 
