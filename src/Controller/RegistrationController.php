d75d123a1aee1cc56aae4995c84b5467bad5a1af
Valory <dzenari18@csndnevers.net>
2023-08-10 14:40:19 +0200

    Update CRUD

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -33,7 +33,7 @@ class RegistrationController extends AbstractController
             $entityManager->flush();
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('app_main_homepage');
+            return $this->redirectToRoute('app_task_index');
         }
 
         return $this->render('registration/register.html.twig', [
